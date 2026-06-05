545bcbe0499548a12f37cb8a07252e1899518df4
Simauniha <2005.simaunihao@gmail.com>
2026-06-06 04:55:00 +0530

    working on navbar almost done

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import "./Navbar.css";
-import logo from "../../assets/logo2.png"; // adjust path if needed
+import { FaCartPlus } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
+import logo from "../../assets/logo2.png";
 
 function Navbar() {
   return (
@@ -19,10 +21,10 @@ function Navbar() {
     // </nav>
 
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
+      <nav className="navbar navbar-expand-lg">
+        <div className="container">
+          <a className="navbar-brand logo" href="/">
+            <img src={logo} alt="ManVibe Logo" />
           </a>
           <button
             className="navbar-toggler"
@@ -33,22 +35,46 @@ function Navbar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
+
             <span className="navbar-toggler-icon" />
           </button>
+
+
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <a className="nav-link active" aria-current="page" href="/">
                   Home
                 </a>
               </li>
+
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
+                <a className="nav-link" href="/shop">
+                  Shop
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="/top-products">
+                  Top Products
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="/new-collection">
+                  New Collection
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="/contact">
+                  Contact
                 </a>
               </li>
             </ul>
-            <ul>
+
+            <ul className="navbar-nav">
               <li className="nav-item dropdown">
                 <a
                   className="nav-link dropdown-toggle"
@@ -56,18 +82,19 @@ function Navbar() {
                   role="button"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
+                  color="white"
                 >
-                  Dropdown
+                  ENG
                 </a>
                 <ul className="dropdown-menu">
                   <li>
                     <a className="dropdown-item" href="#">
-                      Action
+                      English
                     </a>
                   </li>
                   <li>
                     <a className="dropdown-item" href="#">
-                      Another action
+                      Hindi
                     </a>
                   </li>
                   <li>
@@ -75,23 +102,30 @@ function Navbar() {
                   </li>
                   <li>
                     <a className="dropdown-item" href="#">
-                      Something else here
+                      Another
                     </a>
                   </li>
                 </ul>
               </li>
             </ul>
-            <form className="d-flex" role="search">
+            {/* <form className="d-flex search" role="search">
               <input
                 className="form-control me-2"
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
               />
-              <button className="btn btn-outline-success" type="submit">
+              <button className="btn" type="submit">
                 Search
               </button>
-            </form>
+            </form> */}
+
+            <div className="d-flex align-items-center gap-3">
+              <button className="btn icon"><FaSearch /></button>
+              <button className="btn icon"><FaCartPlus /></button>
+              <button className="btn login-btn">Login</button>
+            </div>
+
           </div>
         </div>
       </nav>
