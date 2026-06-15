c22b52ad7ea6998cae16615754570bf1cd57a14a
Simauniha <2005.simaunihao@gmail.com>
2026-06-15 15:45:23 +0530

    footer is done

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,43 +1,59 @@
 import "./Footer.css";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { BsBrowserEdge } from "react-icons/bs";
+import { FaLinkedin } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer-container">
 
-        {/* Left */}
-        <div className="footer-section">
+        <div className="footer-section d-flex flex-column align-items-center text-center gap-2">
           <img src="src/assets/logo2.png" className="logo" alt="logo" />
-          <p>Building modern and responsive web experiences.</p>
+          <h2><strong>ManVibe</strong></h2>
+          <p>Style. Tech. Essentials for everyday life.</p>
         </div>
 
-        {/* Middle */}
         <div className="footer-section">
           <h3>Quick Links</h3>
+          <hr />
           <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/shop">Shop</Link></li>
+            <li><Link to="/top-product">Top Product</Link></li>
+            <li><Link to="/new-collection">New Collection</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
           </ul>
         </div>
 
-        {/* Right */}
         <div className="footer-section">
           <h3>Contact</h3>
-          <p>Email: hello@mysite.com</p>
+          <hr />
+          <p>Email: manvibe@gmail.com</p>
           <p>Phone: +91 98765 43210</p>
+          <p>Location: India</p>
+          <p>Mon–Sat: 9AM – 6PM</p>
         </div>
 
-        <div className="footer-section">
-          <h3>Contact</h3>
-          <p>Email: hello@mysite.com</p>
-          <p>Phone: +91 98765 43210</p>
+        <div className="footer-section section2">
+          <h3>Social Links</h3>
+          <hr />
+          <div className="footer-icon d-flex">
+            <button className="btn icon-btn"><FaFacebook size={22} /></button>
+            <button className="btn icon-btn"><FaInstagram size={22} /></button>
+            <button className="btn icon-btn"><BsBrowserEdge size={22} /></button>
+            <button className="btn icon-btn"><FaLinkedin size={22} /></button>
+          </div>
+
+          <p>Contact Us</p>
         </div>
 
       </div>
 
       <div className="footer-bottom">
-        <p>&copy; 2026 ManVibe. All rights reserved By Simauniha.</p>
+        <p>&copy; 2026 ManVibe. Crafted with passion.</p>
       </div>
     </footer>
   );
